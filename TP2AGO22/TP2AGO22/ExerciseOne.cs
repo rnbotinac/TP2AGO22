2108cbf24e07ce917148c3247e47438d4a0bed8b
ruthnbotinac <30707366+ruthnbotinac@users.noreply.github.com>
2023-08-22 19:04:02 -0500

    Initialize application
    Initialize application

---
+++ TP2AGO22/TP2AGO22/ExerciseOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2AGO22
+{
+    public class ExerciseOne
+    {
+        public static void ExecuteMethod()
+        {
+
+        }
+    }
+}
---
+++ TP2AGO22/TP2AGO22/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TP2AGO22
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Select what exercise should be executed
+            string userValue = "";
+
+            do
+            {
+                switch (userValue)
+                {
+                    case "1":
+                        ExerciseOne.ExecuteMethod();
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (true);
+        }
+    }
+}
